fe5fd2c307177c92905f2ca1c761aded5b3e6f2c
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-04-11 18:51:39 +0700

    Add a model for the menu and menu item
    Also added the display of these models in the admin panel.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, MenuItem
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('menu', 'name', 'url', 'parent')
+    list_filter = ('menu', 'name', 'url', 'parent')
+    search_fields = ('menu', 'name', 'url', 'parent')
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        'self', null=True, blank=True,
+        related_name='children', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
